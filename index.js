00db4e77eb0463f172159ebd9ba20fb94b150bd6
lizheng11 <lizheng11@meituan.com>
2017-07-19 11:14:31 +0800

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * * Created by liz
+ * Email: lizheng11@163.com
+ * Tel: 18686768624
+ * Date: 2017/7/19
+ */
+'use strict';
+
+module.exports = {
+    presets: [],
+    plugins: [
+        require("babel-plugin-syntax-async-functions"),
+        require("babel-plugin-transform-async-to-generator"),
+        require("babel-plugin-transform-class-properties"),
+        require("babel-plugin-transform-decorators-legacy"),
+        require("babel-plugin-transform-es2015-modules-commonjs"),
+        require("babel-plugin-transform-export-extensions"),
+        require("babel-plugin-transform-function-bind"),
+        require("babel-plugin-transform-inline-environment-variables"),
+        require("babel-plugin-transform-node-env-inline"),
+        require("babel-plugin-transform-object-rest-spread")
+    ]
+};
